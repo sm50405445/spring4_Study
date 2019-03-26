bc3662b915f2cd52a0fcede8a22ba7299f9a8087
sm50405445 <46436097+sm50405445@users.noreply.github.com>
2019-03-26 16:29:06 +0900

    spring4 again

---
+++ spring4-chap02/src/main/java/com/lsm/spring4/spring4_chap02/sensor/Sensor.java
@@ -0,0 +1,26 @@
+package com.lsm.spring4.spring4_chap02.sensor;
+
+import java.util.Properties;
+import java.util.Set;
+
+public class Sensor {
+
+	private Properties additionalInfo;
+	private Set<String> agentCodes;
+	
+	public void setAdditionalInfo(Properties additionalInfo) {
+		this.additionalInfo = additionalInfo;
+	}
+	public void setAgentCodes(Set<String> agentCodes) {
+		this.agentCodes = agentCodes;
+	}
+	
+	public int read() {
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		return "Sensor [additionalInfo="+additionalInfo + ", agentCodes=" + agentCodes + "]";
+	}
+}
